7ee4a65798a5bcc57a4e70b5fbc95bc27ae92272
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-12-01 22:54:04 +0700

    🪛

--- javascript/zoom-zoom.js
+++ javascript/zoom-zoom.js
@@ -1,15 +1,18 @@
 onUiLoaded(function () {
-  let imageContainer = document.getElementById("lightboxModal");
-  let modalControls = imageContainer.getElementsByClassName("modalControls")[0];
+  let imageContainer = document.getElementById('lightboxModal');
+  let modalControls = imageContainer.getElementsByClassName('modalControls')[0];
   let DisplayBefore = window.getComputedStyle(imageContainer).display;
-  let img = imageContainer.querySelector("img");
-  var SB = 'var(--primary-400)';
+  let img = imageContainer.querySelector('img');
 
-  const downloadSpan = document.createElement("span");
-  downloadSpan.className = "downloadImage cursor";
-  downloadSpan.innerHTML = "🡇";
-  downloadSpan.title = "Download Image";
+  const modalClose = imageContainer.querySelector('.modalClose');
+
+  const downloadSpan = document.createElement('span');
+  downloadSpan.className = 'downloadImage cursor';
+  downloadSpan.innerHTML = '🡇';
+  downloadSpan.title = 'Download Image';
   modalControls.appendChild(downloadSpan);
+
+  var SB = 'var(--primary-400)';
 
   if (modalControls) {
     downloadSpan.addEventListener("click", function(event) {
@@ -17,7 +20,7 @@ onUiLoaded(function () {
         const imgUrl = img.src;
         const filename = imgUrl.substring(imgUrl.lastIndexOf("/") + 1, imgUrl.lastIndexOf("?"));
         const downloadLink = document.createElement("a");
-        downloadLink.href = img.src;
+        downloadLink.href = imgUrl;
         downloadLink.download = filename;
         document.body.appendChild(downloadLink);
         downloadLink.click();
@@ -34,6 +37,7 @@ onUiLoaded(function () {
         overflow: hidden !important;
         backdrop-filter: blur(10px);
         background-color: rgba(0, 0, 0, 0.7) !important;
+        transition: opacity 0.2s ease;
       }
 
       #lightboxModal > img {
@@ -41,6 +45,8 @@ onUiLoaded(function () {
         height: auto !important;
         max-width: 100%;
         max-height: 100%;
+        transform: translate(0px, 0px) scale(0);
+        transition: transform 0.5s ease;
       }
 
       .modalZoom, .modalSave, .modalTileImage, .modalToggleLivePreview {
@@ -69,6 +75,7 @@ onUiLoaded(function () {
       .modalControls span:hover {
         filter: brightness(0.7);
         text-shadow: 0px 0px 1rem black !important;
+        transform: scale(1.3);
       }
 
       .modalClose {
@@ -88,6 +95,7 @@ onUiLoaded(function () {
         background-color: transparent !important;
         background: transparent !important;
         text-shadow: 0px 0px 1rem black !important;
+        transform: scale(1.3);
       }
 
       .downloadImage {
@@ -100,95 +108,125 @@ onUiLoaded(function () {
   }
 
   let scale = 1;
+  let lastScale = scale;
   let lastX = 0;
   let lastY = 0;
   let offsetX = 0;
   let offsetY = 0;
+  let centerX = 0;
+  let centerY = 0;
+  let delta = 0;
   let Groped = false;
   let GropinTime;
 
-  function imgEL(E) {
+  function imgEL(e) {
     const imgRect = img.getBoundingClientRect();
-    if (E.clientX >= imgRect.left && E.clientX <= imgRect.right &&
-        E.clientY >= imgRect.top && E.clientY <= imgRect.bottom) { 
+    if (e.clientX >= imgRect.left && e.clientX <= imgRect.right &&
+        e.clientY >= imgRect.top && e.clientY <= imgRect.bottom) { 
       return true; 
     }
     return false;
   }
 
-  function imgClick(E) {
-    E.stopPropagation();
+  img.onload = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    imgReset();
+    img.style.transform = 'translate(0px, 0px) scale(1)';
+  };
+
+  img.ondrag = img.ondragend = img.ondragstart = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  img.addEventListener('wheel', (e) => {
+    if (!imgEL(e)) return;
+    e.preventDefault();
+    e.stopPropagation();
+    img.style.transition = 'transform 0.3s ease';
+    centerX = imageContainer.offsetWidth / 2;
+    centerY = imageContainer.offsetHeight / 2;
+    delta = Math.max(-1, Math.min(1, e.wheelDelta || -e.detail));
+    const zoomStep = 0.1;
+    const zoom = 1 + delta * zoomStep;
+    lastScale = scale;
+    scale *= zoom;
+    scale = Math.max(0.1, scale);
+    const imgCenterX = offsetX + centerX;
+    const imgCenterY = offsetY + centerY;
+    offsetX = e.clientX - ((e.clientX - imgCenterX) / lastScale) * scale - centerX;
+    offsetY = e.clientY - ((e.clientY - imgCenterY) / lastScale) * scale - centerY;
+    img.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(${scale})`;
+  });
+
+  img.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+
+    GropinTime = setTimeout(() => {
+      Groped = true;
+      img.style.transition = 'transform 60ms ease';
+      img.style.cursor = 'grab';
+      lastX = e.clientX - offsetX;
+      lastY = e.clientY - offsetY;
+    }, 100);
+  });
+
+  img.addEventListener('mousemove', (e) => {
+    if (!Groped) return;
+    e.preventDefault();
+    img.onclick = (e) => {
+      e.stopPropagation();
+    }
+
+    const deltaX = e.clientX - lastX;
+    const deltaY = e.clientY - lastY;
+    offsetX = deltaX;
+    offsetY = deltaY;
+    img.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${scale})`;
+  });
+
+  img.addEventListener('mouseup', (e) => {
+    clearTimeout(GropinTime);
+
+    if (!Groped) {
+      img.onclick = (e) => {
+        e.stopPropagation();
+        imgClose();
+      }
+      return;
+    }
+
+    Groped = false;
+    img.style.cursor = 'auto';
+    img.style.transition = 'transform 0.3s ease';
+  });
+
+  img.addEventListener('mouseleave', (e) => {
+    if (Groped) {
+      lastX = e.clientX - offsetX;
+      lastY = e.clientY - offsetY;
+    }
+  });
+
+  imageContainer.onclick = modalClose.onclick = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    imgClose();
   }
 
-  if (imageContainer) {
-    img.style.transform = "translate(0px, 0px) scale(0)";
-    img.onload = function () {
+  function imgClose() {
+    document.body.style.overflow = '';
+    imageContainer.style.transition = 'opacity 0.5s ease';
+    img.style.transform = 'translate(0px, 0px) scale(0)';
+    imageContainer.style.opacity = '0';
+
+    setTimeout(() => {
+      imageContainer.style.display = 'none';
       imgReset();
-      img.style.transform = "translate(0px, 0px) scale(1)";
-    };
-
-    img.ondrag = img.ondragend = img.ondragstart = (E) => {
-      E.stopPropagation();
-      E.preventDefault();
-    };
-
-    img.addEventListener("wheel", (E) => {
-      if (!imgEL(E)) return;
-      E.stopPropagation();
-      E.preventDefault();
-      img.style.transition = "transform 0.4s ease";
-      const centerX = imageContainer.offsetWidth / 2;
-      const centerY = imageContainer.offsetHeight / 2;
-      const delta = Math.max(-1, Math.min(1, E.wheelDelta || -E.detail));
-      const zoomStep = 0.1;
-      const zoom = 1 + delta * zoomStep;
-      const lastScale = scale;
-      scale *= zoom;
-      scale = Math.max(0.1, scale);
-      const imgCenterX = offsetX + centerX;
-      const imgCenterY = offsetY + centerY;
-      offsetX = E.clientX - ((E.clientX - imgCenterX) / lastScale) * scale - centerX;
-      offsetY = E.clientY - ((E.clientY - imgCenterY) / lastScale) * scale - centerY;
-      img.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(${scale})`;
-    });
-
-    img.addEventListener("mousedown", (E) => {
-      if (!imgEL(E)) return;
-      E.preventDefault();
-      GropinTime = setTimeout(() => {
-        img.style.cursor = "grab";
-        img.style.transition = "transform 30ms ease";
-        Groped = true;
-        lastX = E.clientX - offsetX;
-        lastY = E.clientY - offsetY;
-      }, 100);
-    });
-
-    img.addEventListener("mousemove", (E) => {
-      if (!imgEL(E) || !Groped) return;
-      E.preventDefault();
-      Groping = true;
-      img.onclick = imgClick;
-      const deltaX = E.clientX - lastX;
-      const deltaY = E.clientY - lastY;
-      offsetX = deltaX;
-      offsetY = deltaY;
-      img.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${scale})`;
-    });
-
-    ['mouseup', 'mouseleave'].forEach((eventType) => {
-      img.addEventListener(eventType, (E) => {
-        clearTimeout(GropinTime);
-        if (!imgEL(E)) return;
-        if (!Groped) {
-          img.onclick = E.stopPropagation();
-          return;
-        }
-        Groped = false;
-        img.style.transition = "transform 0.4s ease";
-        img.style.cursor = "auto";
-      });
-    });
+      imageContainer.style.transition = '';
+      imageContainer.style.opacity = '1';
+    }, 200);
   }
 
   function imgReset() {
@@ -198,41 +236,44 @@ onUiLoaded(function () {
     lastY = 0;
     offsetX = 0;
     offsetY = 0;
-    img.style.transform = "translate(0px, 0px) scale(0)";
-    img.style.transition = "transform 0.4s ease";
     Groped = false;
+    centerX = 0;
+    centerY = 0;
+    delta = 0;
+
+    img.style.transform = 'translate(0px, 0px) scale(0)';
+    img.style.transition = 'transform 0.5s ease';
   }
 
   function toggleNextPrev() {
     const imgGallery = gradioApp().querySelectorAll('div[id^="tab_"] div[id$="_results"] .thumbnail-item > img');
     const imgSrc = new Set(Array.from(imgGallery).map(img => img.src));
-    const imgPrev = imageContainer.querySelector(".modalPrev");
-    const imgNext = imageContainer.querySelector(".modalNext");
+    const imgPrev = imageContainer.querySelector('.modalPrev');
+    const imgNext = imageContainer.querySelector('.modalNext');
 
     if (imgPrev && imgNext) {
       if (imgSrc.size > 1) {
-        imgPrev.style.display = "block";
-        imgNext.style.display = "block";
+        imgPrev.style.display = 'block';
+        imgNext.style.display = 'block';
       } else {
-        imgPrev.style.display = "none";
-        imgNext.style.display = "none";
+        imgPrev.style.display = 'none';
+        imgNext.style.display = 'none';
       }
     }
   }
 
-  function DisplayChange() {
+  const Watcher = new MutationObserver(() => {
     const DisplayNow = window.getComputedStyle(imageContainer).display;
     if (DisplayNow !== DisplayBefore) {
       DisplayBefore = DisplayNow;
-      if (DisplayNow === "flex") {
+      if (DisplayNow === 'flex') {
         toggleNextPrev();
-        document.body.style.overflow = "hidden";
-      } else if (DisplayNow === "none") {
-        document.body.style.overflow = "";
-        imgReset();
+        document.body.style.overflow = 'hidden';
+      } else if (DisplayNow === 'none') {
+        return;
       }
     }
-    requestAnimationFrame(DisplayChange);
-  }
-  requestAnimationFrame(DisplayChange);
+  });
+
+  Watcher.observe(imageContainer, { attributes: true, attributeFilter: ['style'] });
 });
