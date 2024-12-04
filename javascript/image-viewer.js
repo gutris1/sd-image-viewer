88bf6b164c74d8ccf6f1ee1e7c51099d2a9bcb75
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-12-04 16:41:22 +0700

    🍊

--- javascript/image-viewer.js
+++ javascript/image-viewer.js
@@ -1,18 +1,29 @@
 onUiLoaded(function () {
   let imageContainer = document.getElementById('lightboxModal');
   let modalControls = imageContainer.getElementsByClassName('modalControls')[0];
+  let modalClose = imageContainer.querySelector('.modalClose');
   let DisplayBefore = window.getComputedStyle(imageContainer).display;
   let img = imageContainer.querySelector('img');
-
-  const modalClose = imageContainer.querySelector('.modalClose');
 
   const downloadSpan = document.createElement('span');
   downloadSpan.className = 'downloadImage cursor';
   downloadSpan.title = 'Download Image';
   downloadSpan.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 32 32">
-      <path fill="currentColor" stroke="currentColor" stroke-width="1.8" d="M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4zm0-10l-1.41-1.41L17 20.17V2h-2v18.17l-7.59-7.58L6 14l10 10l10-10z"></path>
-    </svg>`;
+    <svg xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        viewBox="0 0 32 32">
+      <path
+        fill="currentColor"
+        stroke="currentColor"
+        stroke-width="1.8"
+        d="
+          M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4zm0-10
+          l-1.41-1.41L17 20.17V2h-2v18.17l-7.59-7.58L6 14l10 10l10-10z
+        ">
+      </path>
+    </svg>
+    `;
   modalControls.appendChild(downloadSpan);
 
   var SB = 'var(--primary-400)';
