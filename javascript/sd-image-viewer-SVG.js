730d3e05fbf91f66416e373509938d7a17e4a121
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-04-02 23:03:57 +0700

    Create sd-image-viewer-SVG.js

---
+++ javascript/sd-image-viewer-SVG.js
@@ -0,0 +1,55 @@
+var SDImageViewerStaticSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' width='30px' height='30px' viewBox='0 0 24 24' fill='transparent'
+    stroke='currentColor' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'>
+    <rect x='0.802' y='0.846' width='22.352' height='22.352' rx='2' ry='2'/>
+    <circle cx='7.632' cy='7.676' r='1.862'/>
+    <polyline points='23.154 15.747 16.946 9.539 3.285 23.198'/>
+  </svg>
+`;
+
+var SDImageViewerLiveSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' width='32px' height='32px' viewBox='0 0 24 24' fill='transparent'
+    stroke='currentColor' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'>
+    <path d='M 7.263 3.393 L 7.263 2.803 M 7.263 2.803 C 7.263 1.698 8.158 0.803 9.263
+      0.803 L 21.153 0.803 C 22.258 0.803 23.153 1.698 23.153 2.803 L 23.153 14.693 C 23.153
+      15.798 22.258 16.693 21.153 16.693 M 21.153 16.693 L 20.251 16.693'/>
+    <path d='M 4.098 5.924 C 4.098 4.819 4.993 3.924 6.098 3.924 L 17.988 3.924 C 19.093
+      3.924 19.988 4.819 19.988 5.924 L 19.988 17.814 C 19.988 18.919 19.093 19.814 17.988
+      19.814 L 16.813 19.814 M 4.098 7.138 L 4.098 5.924'/>
+    <path d='M 2.802 7.308 H 14.692 A 2 2 0 0 1 16.692 9.308 V 21.198 A 2 2 0 0 1 14.692
+      23.198 H 2.802 A 2 2 0 0 1 0.802 21.198 V 9.308 A 2 2 0 0 1 2.802 7.308 Z'/>
+    <circle cx='5.658' cy='12.163' r='1.324'/>
+    <polyline points='16.692 17.901 12.279 13.488 2.567 23.198'/>
+  </svg>
+`;
+
+var SDImageViewerDownloadSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32'>
+    <path fill='currentColor' stroke='currentColor' stroke-width='1.8'
+      d='M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4zm0-10
+      l-1.41-1.41L17 20.17V2h-2v18.17l-7.59-7.58L6 14l10 10l10-10z'/>
+  </svg>
+`;
+
+var SDImageViewerPrevSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='32px' height='32px' viewBox='0 0 24 24'>
+    <path d='m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z'/>
+  </svg>
+`;
+
+var SDImageViewerNextSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='32px' height='32px' viewBox='0 0 24 24'>
+    <path d='M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z'/>
+  </svg>
+`;
+
+var SDImageViewerCloseSVG = `
+  <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' width='32px' height='32px' viewBox='0 0 512 512'>
+    <path fill='currentColor' d='M330.443 256l136.765-136.765c14.058-14.058 14.058-36.85
+      0-50.908l-23.535-23.535c-14.058-14.058-36.85-14.058-50.908 0L256 181.557L119.235
+      44.792c-14.058-14.058-36.85-14.058-50.908 0L44.792 68.327c-14.058 14.058-14.058
+      36.85 0 50.908L181.557 256L44.792 392.765c-14.058 14.058-14.058 36.85 0 50.908l23.535
+      23.535c14.058 14.058 36.85 14.058 50.908 0L256 330.443l136.765 136.765c14.058 14.058
+      36.85 14.058 50.908 0l23.535-23.535c14.058-14.058 14.058-36.85 0-50.908L330.443 256z'/>
+  </svg>
+`;
